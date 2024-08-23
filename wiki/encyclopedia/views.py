03504efed3f859387e128eb64ec9473a53ec0377
gasper1 <gasper.kk@gmail.com>
2024-08-23 17:26:46 +0200

    Project 1 in progress
    Proj1 - Entry page, index and search specs done

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from markdown2 import Markdown
+from django.shortcuts import redirect
+from django.urls import reverse
 
 from . import util
 
@@ -6,5 +9,22 @@ from . import util
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
+    })
+
+def wiki(request, title):
+    markdowner = Markdown()
+    return render(request, "encyclopedia/wiki.html", {
+        "title": title,
+        "content": markdowner.convert(util.get_entry(title))
+    })
+
+def search(request):
+    query = request.GET.get('q','')
+    entries = [entry for entry in util.list_entries() if query.lower() in entry.lower()]
+    if len(entries) == 1:
+        return redirect("/wiki/" + entries[0])
+    return render(request, "encyclopedia/search.html", {
+        "query": query, 
+        "entries": entries
     })
 
