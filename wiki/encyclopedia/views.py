d0eee44d8e1df8171cfe2a15a290c6a4290c54bd
gasper1 <gasper.kk@gmail.com>
2024-08-24 11:15:23 +0200

    In Progress - random done, TODO new and edit

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from markdown2 import Markdown
 from django.shortcuts import redirect
 from django.urls import reverse
+from django.core.files import File
 
 from . import util
+
+from random import randint
 
 
 def index(request):
@@ -26,5 +29,37 @@ def search(request):
     return render(request, "encyclopedia/search.html", {
         "query": query, 
         "entries": entries
+    })
+
+def new(request):
+    page_title, page_content = '', ''
+    if request.method =='POST':
+        page_title = request.POST.get('page_title','')
+        page_content = request.POST.get('page_content','')   
+
+    #TODO check if entry is already stored
+    
+    #TODO if not, create a new .md file in the entries page
+    
+
+    return render(request, "encyclopedia/new.html", {
+        'page_title': page_title,
+        'page_content': page_content
+    })
+
+def edit(request, title):
+    return render(request, "encyclopedia/edit.html", {
+        "page_title": title,
+        "page_content": util.get_entry(title)
+    })
+
+def random(request):
+    markdowner = Markdown()
+    entries = util.list_entries()
+    rand_idx = randint(0, len(entries)-1)
+    title = entries[rand_idx]
+    return render(request, "encyclopedia/wiki.html", {
+        "title": title,
+        "content": markdowner.convert(util.get_entry(title))
     })
 
