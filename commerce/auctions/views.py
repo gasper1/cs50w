5b4ea87c72ee3634377f2eaf82d60835ca1d0b9a
gasper1 <gasper.kk@gmail.com>
2024-09-02 12:09:13 +0200

    Bid featured - done + fix on index view

--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 from django import forms
+from django.db.models import Max
 
 from .models import User, Listing, ListingCategories, Bid
 
@@ -16,14 +17,20 @@ class CreateForm(forms.Form):
 
 
 class BidForm(forms.Form):
-    price = forms.FloatField()
+    def __init__(self, *args, **kwargs):
+        max_bid = kwargs.pop('max_bid', 0)  # Pop the max_bid from kwargs, default to 0 if not provided
+        super().__init__(*args, **kwargs)  # Call the parent constructor
+        self.fields['price'].min_value = max_bid + 0.01  # Dynamically set the min_value
+        self.fields['price'].widget.attrs['min'] = max_bid + 0.01  # Set the HTML min attribute for client-side validation
+
+    price = forms.FloatField(min_value=0.01)
 
 
 def index(request):
     listings = []
     if request.user.is_authenticated:
         user = User.objects.get(pk=request.user.id)
-        listings = user.listings.all()
+        listings = Listing.objects.all()
 
     return render(request, "auctions/index.html", {
         "listings": listings
@@ -33,24 +40,27 @@ def index(request):
 def item(request, id):
     user = User.objects.get(pk=request.user.id)
     listing = Listing.objects.get(pk=id)
-    bid_form = BidForm()
+    max_bid = listing.bids.aggregate(Max("price", default=listing.starting_price))["price__max"]
+    bid_form = BidForm(max_bid=max_bid)
     starting_price_bid = Bid(listing=listing, price=listing.starting_price, by_user=listing.by_user, created_at=listing.created_at)
     return render(request, "auctions/item.html", {
         "listing": listing,
         "watched_by_user": user in listing.watchlisted_by.all(),
         "bid_form": bid_form,
-        "bids": [starting_price_bid] + [bid for bid in Bid.objects.filter(by_user=user, listing=listing)]
+        "bids": [starting_price_bid] + [bid for bid in Bid.objects.filter(listing=listing)]
     })
 
 
 def place_bid(request, id):
     if request.method == "POST":
         user = User.objects.get(pk=request.user.id)
-        form = BidForm(request.POST)
-        if form.is_valid():
+        listing = Listing.objects.get(pk=id)
+        max_bid = listing.bids.aggregate(Max("price", default=listing.starting_price))["price__max"]
+        form = BidForm(request.POST, max_bid=max_bid)
+        if form.is_valid() and user != listing.by_user:
             bid = Bid(
                 by_user = user,
-                listing = Listing.objects.get(pk=id),
+                listing = listing,
                 price = form.cleaned_data["price"]
             )
             bid.save()
